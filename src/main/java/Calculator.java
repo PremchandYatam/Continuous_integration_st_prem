2423789f29daeaf41fadfe7031616c9e010a9875
PremChand <premchands10@gmail.com>
2021-06-09 21:51:45 +0200

    environment

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public Calculator(){
+
+    }
+    public int add(int a, int b){
+        int c= a+b;
+        return c;
+    }
+    public int sub(int d, int e){
+        int f= d-e;
+        return f;
+    }
+    public int mul(int g, int h){
+        int i= g*h;
+        return i;
+    }
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestCalculator {
+    private static Calculator obj;
+
+    @BeforeClass
+    public static void setup(){
+        obj= new Calculator();
+    }
+    @Test
+    public void test_add(){
+        assertEquals(3, obj.add(1,2));
+    }
+    @Test
+    public void test_sub(){
+        assertEquals(1, obj.sub(2,1));
+    }
+    @Test
+    public void test_mul(){
+        assertEquals(20, obj.mul(4,5));
+    }
+}
